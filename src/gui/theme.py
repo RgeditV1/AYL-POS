e5a20b310df9a62ff9e99466b06313fa4466fc91
Rgeditv1 <rgeditv1@protonmail.com>
2026-03-28 18:39:07 -0400

    feat: update light theme to use custom color scheme and comfortable visual density

--- src/gui/theme.py
+++ src/gui/theme.py
@@ -28,4 +28,17 @@ class ThemeManager:
 
     @staticmethod
     def get_light_theme():
-        return ft.Theme(color_scheme_seed=ft.Colors.BLUE)
+        return ft.Theme(
+            color_scheme=ft.ColorScheme(
+                primary=SKY_BLUE_400,
+                on_primary=DARK_SLATE_900,
+                secondary=EMERALD_500,
+                on_secondary=SLATE_50,
+                surface=DARK_SLATE_800,
+                on_surface=SLATE_50,
+                surface_container=DARK_SLATE_900,
+                error=ERROR_RED,
+                outline="#334155",
+            ),
+            visual_density=ft.VisualDensity.COMFORTABLE,
+        )
